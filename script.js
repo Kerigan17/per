4603e569ee13da1d8bcb868a65de5f7f634fdd53
alexandr <alexsandr77867@gmail.com>
2024-08-15 20:54:06 +0400

    feat: finish all adaptive

--- script.js
+++ script.js
@@ -4,26 +4,33 @@ const modalPythonElement = document.getElementById('modal-python');
 const modalWebElement = document.getElementById('modal-web');
 const closePythonElement = document.getElementById('close-python');
 const closeWebElement = document.getElementById('close-web');
+const bodyElement = document.getElementsByTagName('body')[0];
 
 pythonElement.addEventListener('click', () => {
     modalPythonElement.classList.add('active');
+    bodyElement.style.overflowY = 'hidden';
 });
 webElement.addEventListener('click', () => {
     modalWebElement.classList.add('active');
+    bodyElement.style.overflowY = 'hidden';
 });
 
 closePythonElement.addEventListener('click', () => {
-    modalPythonElement.classList.remove('active')
+    modalPythonElement.classList.remove('active');
+    bodyElement.style.overflowY = 'scroll';
 });
 closeWebElement.addEventListener('click', () => {
     modalWebElement.classList.remove('active')
+    bodyElement.style.overflowY = 'scroll';
 });
 
 modalPythonElement.addEventListener('click', () => {
-    modalPythonElement.classList.remove('active')
+    modalPythonElement.classList.remove('active');
+    bodyElement.style.overflowY = 'scroll';
 });
 modalWebElement.addEventListener('click', () => {
-    modalWebElement.classList.remove('active')
+    modalWebElement.classList.remove('active');
+    bodyElement.style.overflowY = 'scroll';
 });
 
 document.querySelector('.modal__python').addEventListener('click', (event) => {
